cfeaf33f5b28d36083b5eb98ec0b4b4aa231011f
vagrant <tpirozzini@gmail.com>
2022-04-12 02:07:33 +0000

    add spec/product_spec

---
+++ spec/product_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+
+  describe 'Validations' do
+
+    before do
+      @category = Category.new(name: 'Tools')
+      @category.save
+    end
+    # validation tests/examples here
+    it 'creates a new product' do
+      @product = @category.products.new(name: 'Whack-Hammer', price: 45, quantity: 8)
+      expect(@product).to be_valid
+    end
+
+    it 'throws an error if no category is selected' do
+      @product = Product.create(name: 'Whack-Hammer', price: 45, quantity: 8)
+
+      expect(@product.errors.full_messages).to include("Category can't be blank")
+    end
+
+    it 'throws an error if no category is selected' do
+      @product = Product.create(name: 'Whack-Hammer', quantity: 8)
+
+      expect(@product.errors.full_messages).to include("Price can't be blank")
+    end
+
+    it 'throws an error if no quanity is provided' do
+      @product = Product.create(name: 'Whack-Hammer', price: 45)
+
+      expect(@product.errors.full_messages).to include("Quantity can't be blank")
+    end
+
+  end
+end
