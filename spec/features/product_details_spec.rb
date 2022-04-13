63139dc5bc61bfdeb2195b0c5be0218042e5f14c
vagrant <tpirozzini@gmail.com>
2022-04-13 01:20:06 +0000

    add feature/testing-product-page

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.feature "ProductDetails", type: :feature, js: true do
+
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "They can navigate from the home page to the product detail page by clicking on a product" do
+    # ACT
+    visit root_path 
+    page.first('article.product a h4').click
+
+    # DEBUG / VERIFY
+    save_screenshot
+    expect(page).to have_css 'article.product-detail'
+  end
+  
+end
